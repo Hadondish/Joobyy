d51fdcf10d31e3d28ddfd4e2a29096f65019c2a7
Kyle Tran <48851847+kyletran77@users.noreply.github.com>
2022-03-04 03:39:21 -0800

    Profile Changes Picture

--- tinder-clone/ui/profile/ProfileView.swift
+++ tinder-clone/ui/profile/ProfileView.swift
@@ -46,110 +46,100 @@ struct ProfileView: View {
     @State private var initialOrientation: Orientation = .both
     
     @State private var isLoading: Bool = false
+    @State private var pictures: [UIImage] = []
+    @State private var image = UIImage()
+    @State private var previousPicCount: Int = 0
     @State private var userName: String = ""
     @State private var userBirthdate: String = ""
     @State private var userBio: String = ""
     @State private var showError: Bool = false
 
     
-    @State private var items: [Item] = [.init(name: "Open to work", details: "iOS Developer roles"), .init(name: "Providing Services", details: "iOS Development and Mobile Application Development")]
+    @State private var items: [Item] = [.init(name: "Favorite Songs", details: "Hot or Cold"), .init(name: "Providing Services", details: "iOS Development and Mobile Application Development")]
     
     @State private var currentImageIndex: Int = 0
-
+    
     var body: some View {
         //beginning Vstack
+        ScrollView{
         VStack {
             
-            ZStack {
-                GeometryReader { proxy in
-                    Image("cover")
-                        .resizable()
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100, maxHeight: 100)
-                    
-                    Button {} label: {
-                        Image(systemName: "pencil")
-                            .resizable()
-                            .frame(width: 15, height: 15)
-                            .font(.title)
-                            .background(
-                                Circle()
-                                    .foregroundColor(.white)
-                                    .frame(width: 30, height: 30)
-                            )
-                    }
-                    .offset(x: proxy.size.width - 35, y: 25)
-                    
+//            ZStack {
+//                GeometryReader { proxy in
+//                    Image(uiImage: pictures[0])
+//                        .resizable()
+//                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100, maxHeight: 100)
+//
+//                    Button {} label: {
+//                        Image(systemName: "pencil")
+//                            .resizable()
+//                            .frame(width: 15, height: 15)
+//                            .font(.title)
+//                            .background(
+//                                Circle()
+//                                    .foregroundColor(.white)
+//                                    .frame(width: 30, height: 30)
+//                            )
+//                    }
+//                    .offset(x: proxy.size.width - 35, y: 25)
+//
+//                }
+//            }
+            ZStack(alignment: .bottom){
+                GeometryReader{ geometry in
+                    Image(uiImage: image)
+                        .centerCropped()
+//                        .gesture(DragGesture(minimumDistance: 0).onEnded({ value in
+//                            if value.translation.equalTo(.zero){
+//                                if(value.location.x <= geometry.size.width/2){
+//                                    showPrevPicture()
+//                                } else { showNextPicture()}
+//                            }
+//                        }))
                 }
+                
+                
             }
-            Spacer(minLength: 50)
+            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .aspectRatio(0.7, contentMode: .fit)
+            .background(.white)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            Spacer(minLength: 20)
             makeBottomView()
                 .padding(.horizontal)
-        }
+        }}
     }
     
     func makeBottomView() -> some View {
         VStack(alignment: .leading, spacing: 10) {
-            HStack {
-                ZStack{
-                    Image("56")
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(Circle())
-                        .frame(width: 100, height: 100)
-                        .overlay(Circle().stroke(Color.white,lineWidth:2))
-                    Button {} label: {
-                        Image(systemName: "plus")
-                            .foregroundColor(.black)
-                            .background(
-                                Circle()
-                                    .foregroundColor(.white)
-                                    .frame(width: 30, height: 30)
-                                    .overlay(Circle().stroke(Color.gray,lineWidth: 0.5))
-                            )
-                    }
-                    .offset(x: 30, y: 30)
-                }
-                Spacer()
-                Button {} label: {
-                    Image(systemName: "pencil")
-                        .font(.title)
-                        .foregroundColor(.black)
-                }
-                .offset(y: 20)
-                
-            }
+            
             VStack(alignment: .leading) {
                 HStack {
                     Text(userName)
-                        .font(.title2)
-                    Image(systemName: "speaker.wave.2.fill")
+                        .font(.system(size: 70))
+                        .bold()
                 }
-                Text(userName)
-                    .font(.subheadline)
-                Text("Talks about #swift, #swiftui, and #iosdevelopment")
-                    .font(.caption)
-                    .foregroundColor(.gray)
+                Text("ENTJ")
+                    .font(.largeTitle)
+                    .bold()
+                    .italic()
+
+//                Text("Talks about #swift, #swiftui, and #iosdevelopment")
+//                    .font(.caption)
+//                    .foregroundColor(.gray)
             }
-            
+            Spacer()
+            Spacer()
             VStack(alignment: .leading) {
-                Text("LEAN TRANSITION SOLUTIONS - LTS")
-                    .font(.subheadline)
-                Text("Thiruvananthapuram, Kerala, India")
+                Text(userBio)
+                    .font(.title)
+                Text("")
                     .font(.caption)
                     .foregroundColor(.gray)
             }
-            
-            HStack {
-                Text(UID)
-                    .font(.subheadline)
-                    .foregroundColor(.blue)
-                Circle()
-                    .frame(width: 2, height: 2)
-                Text("500+ connections")
-                    .font(.subheadline)
-                    .foregroundColor(.blue)
-            }
-            
+
             
             makeButtonView()
             
@@ -167,6 +157,17 @@ struct ProfileView: View {
     }
     
     private func performOnAppear(){
+        firestoreViewModel.fetchMainPicture(profileId: UID){
+            result in
+                switch(result){
+                case .success(let user):
+                    image = user
+                    return
+                case .failure(_):
+                    userName = "Failed Lol Loser";
+                    return
+                }}
+
     firestoreViewModel.fetchUserProfile(fetchedUserId: UID){ result in
         switch(result){
         case .success(let user):
@@ -177,8 +178,30 @@ struct ProfileView: View {
             return
             
         }
+
             
-    }}
+    }
+    
+        firestoreViewModel.fetchUserPictures(onCompletion: { result in
+            switch result{
+            case .success(let pictureList):
+                pictures = pictureList
+                previousPicCount = pictureList.count
+                return
+            case .failure(_):
+                return
+            }
+        }, onUpdate: {result in
+            switch result{
+            case .success(let pictureList):
+                pictures = pictureList
+                previousPicCount = pictureList.count
+                return
+            case .failure(_):
+                return
+            }
+        })
+    }
     private func populateData(_ user: FirestoreUser){
         userBio = user.bio
         userName = user.name
@@ -187,6 +210,17 @@ struct ProfileView: View {
         initialOrientation = user.orientation
         self.initialBio = userBio
         self.initialOrientation = user.orientation
+    }
+    private func showNextPicture(){
+        if currentImageIndex < pictures.count - 1 {
+            currentImageIndex += 1
+        }
+    }
+    
+    private func showPrevPicture(){
+        if currentImageIndex > 0 {
+            currentImageIndex -= 1
+        }
     }
     
     func makeCell(for item: Item) -> some View {
@@ -248,6 +282,7 @@ struct ProfileView: View {
             .frame(width: 30)
         }
         .frame(height: 30)
+    
     }
 }
 
