f7d35c0af4cf1feda30fbbd51ba0dffb561606a4
Kyle Tran <48851847+kyletran77@users.noreply.github.com>
2022-04-24 02:09:45 -0700

    Updated Logos and purple Color

--- tinder-clone/ui/common/AppColor.swift
+++ tinder-clone/ui/common/AppColor.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 class AppColor{
     
-    //Pastel Green -
+    //Pastel Purple - #D337CA
     
     static let dislikeColors = [Color(hex: "ff6560"), Color(hex: "f83770")]
     static let likeColors = [Color(hex: "6ceac5"), Color(hex: "16dba1")]
-    static let appColors = [Color(hex: "BFEEB7"), Color(hex: "77dd77")]
+    static let appColors = [Color(hex: "#D337CA"), Color(hex: "##EC73E5")]
     static let purpleColors = [Color(hex: "831bfc"),Color(hex: "9c59ea")]
     
     static let appRed = Color(hex: "ff4457")
